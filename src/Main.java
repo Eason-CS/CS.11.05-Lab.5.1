c4f09022f3e3398932a9b846aee0e85fccd6d365
Eason-CS <easonh2027@student.cis.edu.hk>
2024-10-28 09:06:16 +0800

    Merge remote-tracking branch 'origin/main'

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     /**
@@ -79,7 +81,15 @@ public class Main {
 
     public static int medianFoodServiceEstimateKgPerCapitaPerYear(int[] foodServiceEstimatesKgPerCapitaPerYear)
     {
-        return -1;
+        int[] array = foodServiceEstimatesKgPerCapitaPerYear;
+        Arrays.sort(array);
+        int length = array.length;
+        if (length % 2 == 0) {
+            return (int) (array[length / 2 - 1] + array[length / 2] + 0.5) / 2;
+        } else {
+            return array[length/2];
+        }
+
     }
 
     public static int medianFoodServiceEstimateTonnesPerYear(int[] foodServiceEstimatesTonnesPerYear)
